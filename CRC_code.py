cc1378aba49c57ad6920fc3065a8ed497a7b4ddc
abdulrahman <abdo.thedarkside@gmail.com>
2018-11-21 22:17:10 +0200

    Modifing the indentation of alter function

--- CRC_code.py
+++ CRC_code.py
@@ -47,10 +47,10 @@ def verifier(transmitted,gen):               #verifier function which takes mess
         return ("Message is not correct")
       
       
- def alter(transmitted,bitNO):   #alter function which invert a certain bitNO in the transmitted message 
+def alter(transmitted,bitNO):   #alter function which invert a certain bitNO in the transmitted message 
                                             
-    transmitted = int(transmitted,2)
-    transmitted &= ~(1 << bitNO) # Compute mask, an integer with just bit 'index' set.
-    return str((bin(transmitted))[2:]) # to output "00101010" not "0b00101010"
+   transmitted = int(transmitted,2)
+   transmitted &= ~(1 << bitNO) # Compute mask, an integer with just bit 'index' set.
+   return str((bin(transmitted))[2:]) # to output "00101010" not "0b00101010"
     
     
